4b63023bb0823bc9725c372db6ec9bbcf7e30a01
SkylerOnRadio <abhigyadulal@gmail.com>
2026-06-08 21:30:48 +0530

    Added category member variable to Class of Task and updated the constructors for it

--- tasks.hpp
+++ tasks.hpp
@@ -8,12 +8,13 @@ public:
   std::string task;
   bool completed{false};
   int id;
+  std::string category;
   time_t created_at;
   time_t modified_at;
 
-  Tasks(std::string task, int id);
+  Tasks(std::string task, int id, std::string category = "None");
   Tasks(std::string task, bool complete, int id, time_t created_at,
-        time_t modified_at);
+        time_t modified_at, std::string category = "None");
 };
 
 #endif
