6259b8a484c1575b7974e4f76c86b97e647f58fa
SkylerOnRadio <abhigyadulal@gmail.com>
2026-06-25 17:23:50 +0530

    Loading data from CSV file in functional

--- fileHandling.cpp
+++ fileHandling.cpp
@@ -1,7 +1,86 @@
 #include "header_files/fileHandling.h"
 #include "header_files/taskClass.h"
+#include <ctime>
 #include <fstream>
+#include <iomanip>
+#include <memory>
+#include <sstream>
 #include <string>
+#include <string_view>
+
+std::unique_ptr<Tasks> parseLineToTask(std::string_view line) {
+  int quoteCount{0};
+  int i{1};
+  std::string_view field;
+
+  int id;
+  std::string task;
+  std::string category;
+  int status;
+  bool renewing;
+  time_t created_at;
+  time_t modified_at;
+
+  int startPoint{0};
+  int endPoint{0};
+
+  for (char ch : line) {
+    if (ch == ',' && !(quoteCount & 1)) {
+      field = line.substr(startPoint, endPoint - startPoint);
+      switch (i) {
+      case 1:
+        id = std::stoi(static_cast<std::string>(field));
+        break;
+
+      case 2:
+        task = static_cast<std::string>(field);
+        break;
+
+      case 3:
+        category = static_cast<std::string>(field);
+        break;
+
+      case 4:
+        renewing = field == "false" ? false : true;
+        break;
+
+      case 5:
+        status = std::stoi(static_cast<std::string>(field));
+        break;
+
+      case 6: {
+        std::istringstream ss{static_cast<std::string>(field)};
+        struct std::tm tmp_time{0};
+        ss >> std::get_time(&tmp_time, "%Y-%m-%d:%H-%M-%S");
+        if (!ss.fail())
+          created_at = mktime(&tmp_time);
+        break;
+      }
+
+      case 7: {
+        std::istringstream ss{static_cast<std::string>(field)};
+        struct std::tm tmp_time{0};
+        ss >> std::get_time(&tmp_time, "%Y-%m-%d:%H-%M-%S");
+        if (!ss.fail())
+          modified_at = mktime(&tmp_time);
+        break;
+      }
+
+      default:
+        break;
+      }
+
+      startPoint = endPoint + 1;
+      i++;
+    }
+    if (ch == '"')
+      quoteCount++;
+    endPoint++;
+  }
+
+  return std::make_unique<Tasks>(
+      Tasks(id, task, status, renewing, created_at, modified_at, category));
+}
 
 int loadFile(std::string filename,
              std::vector<std::unique_ptr<Tasks>> &tasksMain) {
@@ -11,5 +90,8 @@ int loadFile(std::string filename,
 
   std::string line;
   while (std::getline(file, line)) {
+    tasksMain.push_back(parseLineToTask(line));
   }
+
+  return 0;
 }
