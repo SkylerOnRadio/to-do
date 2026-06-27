d89071e31d8c69e8378a316f10f94a94bff49928
SkylerOnRadio <abhigyadulal@gmail.com>
2026-06-27 17:43:17 +0530

    added global variables

---
+++ global.cpp
@@ -0,0 +1,10 @@
+#include "header_files/global.h"
+#include <string>
+#include <vector>
+
+int start_index_unique{0};
+int current_index_unique{0};
+std::vector<std::string> activeFilters_unique;
+bool updateTasks_unique{false};
+Tasks *startTask_unique;
+Tasks *lastTask_unique;
---
+++ header_files/global.h
@@ -0,0 +1,14 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+#include "taskClass.h"
+#include <string>
+#include <vector>
+extern int start_index_unique;
+extern int current_index_unique;
+extern std::vector<std::string> activeFilters_unique;
+extern bool updateTasks_unique;
+extern Tasks *startTask_unique;
+extern Tasks *lastTask_unique;
+
+#endif // !GLOBAL_H
