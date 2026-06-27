e5fc6694f24fd8f4b595f876daeeb24ec17f44d5
SkylerOnRadio <abhigyadulal@gmail.com>
2026-06-27 21:32:58 +0530

    added --updateFile as a flag to update the files of the previous major version

--- main.cpp
+++ main.cpp
@@ -7,11 +7,13 @@
 #include <memory>
 #include <ncurses.h>
 #include <string>
+#include <string_view>
 #include <vector>
 
 #define DEBUG
 
-void parseArguments(char *arguments[], int argCount) {
+void parseArguments(char *arguments[], int argCount,
+                    std::string_view filename) {
   for (int i = 1; i < argCount; ++i) {
     if (strcmp(arguments[i], "-h") == 0 ||
         strcmp(arguments[i], "--help") == 0) {
@@ -22,6 +24,9 @@ void parseArguments(char *arguments[], int argCount) {
           << "--updateFile  : updates the task.csv file that the previous "
              "version uses to make it compatible with the new version.\n";
       exit(0);
+    } else if (strcmp(arguments[i], "--updateFile") == 0) {
+      updatePreviousVersionFile(static_cast<std::string>(filename));
+      exit(EXIT_SUCCESS);
     } else {
       std::cout
           << "Invalid flag. Use -h or --help to find flags and their uses.\n";
@@ -30,11 +35,6 @@ void parseArguments(char *arguments[], int argCount) {
 }
 
 int main(int argc, char *argv[]) {
-  // checking if arguments are passed, if yes let the argument parser handle
-  // them
-  if (argc > 1)
-    parseArguments(argv, argc);
-
   std::string filename{""};
 #ifndef DEBUG
   // checking if HOME directory exits
@@ -47,11 +47,16 @@ int main(int argc, char *argv[]) {
     exit(EXIT_FAILURE);
   }
   filename = std::string(homeDir) + "/.tasks.csv";
-#endif // !DEBUG
+#endif
 
 #ifdef DEBUG
   filename = "test.csv";
-#endif // DEBUG
+#endif
+
+  // checking if arguments are passed, if yes let the argument parser handle
+  // them
+  if (argc > 1)
+    parseArguments(argv, argc, filename);
 
   std::vector<std::unique_ptr<Tasks>> tasks;
 
